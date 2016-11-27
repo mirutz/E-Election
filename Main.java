61d1aed3e2e782d2b9219c343db235043afd5645
Moritz Malburg <mirutz12@gmail.com>
2016-11-27 23:15:57 +0100

    main vergessen...

---
+++ Main.java
@@ -0,0 +1,11 @@
+package draft;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AusgabeWahlkarten ausg = new AusgabeWahlkarten();
+        ausg.ausgabe();
+
+    }
+
+}
